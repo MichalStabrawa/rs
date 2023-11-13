52c7812e0fc7934ead6d37bacc74ef5368f3b0bd
Michal Stabrawa <mistaaqua@gmail.com>
2023-11-13 22:08:15 +0100

    add ondecrease and onIncrease to modal

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,11 +4,13 @@ import { currencyFormatter } from "../util/formatting";
 import Button from "./Ui/Button";
 import { useContext } from "react";
 import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext)
-  console.log(userProgressCtx);
+  console.log('ITEMS')
+  console.log(cartCtx.items.length);
 
   const cartTotal = cartCtx.items.reduce((totalPrice,item)=>totalPrice+ item.quantity*item.price,0)
 
@@ -22,15 +24,14 @@ export default function Cart() {
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+        <CartItem key={item.id} name={item.name} price ={item.price} quantity={item.quantity} onDecrease={()=>cartCtx.removeItem(item.id)} onIncrease={()=>cartCtx.addItem(item)}></CartItem>
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
         <Button textOnly onClick={handleCartClose}>Close</Button>
-        <Button>Go to checkout</Button>
+        {cartCtx.items.length !==0 && <Button>Go to checkout</Button>}
+       
       </p>
     </Modal>
   );

b2a2a3d187b564f5b52891f760cccc227895d9ec
Michal Stabrawa <mistaaqua@gmail.com>
2023-11-14 22:46:12 +0100

    add handle submit form

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -8,30 +8,56 @@ import UserProgressContext from "../store/UserProgressContext";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
-  const userProgressCtx = useContext(UserProgressContext)
+  const userProgressCtx = useContext(UserProgressContext);
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
   function handleClose() {
-    userProgressCtx.hideCheckout()
+    userProgressCtx.hideCheckout();
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const url = "http://localhost:3000/orders";
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+    console.log(customerData);
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items:cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
-    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
-      <form>
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount:{currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-MAIL Adress" type="email" id="email" />
+        <Input label="Street" type="text" id="street"></Input>
         <div className="control-row">
           <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
         </div>
         <p className="modal-actions">
-            <Button type="button" textOnly onClick={handleClose}>Close</Button>
-            <Button>Submit Order</Button>
+          <Button type="button" textOnly onClick={handleClose}>
+            Close
+          </Button>
+          <Button>Submit Order</Button>
         </p>
       </form>
     </Modal>

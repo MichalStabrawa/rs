b0cfb6c4ba966308b09282e8eb73c7f7f7e68ec1
Michal Stabrawa <mistaaqua@gmail.com>
2023-11-27 23:22:42 +0100

    add error component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
+import Error from "./Error";
 
 const requestConfig = {};
 
@@ -13,7 +14,11 @@ export default function Meals() {
   console.log(loadedMeals)
 
   if (isLoading) {
-    return <p>Fetching meals.....</p>;
+    return <p className="center">Fetching meals.....</p>;
+  }
+
+  if(error) {
+    return <Error title="Failed to fetch meals" message={error}/>
   }
   return (
     <ul id="meals">
